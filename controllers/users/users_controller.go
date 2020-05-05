87fcb4c556d9fc0484ddaab70c81d547cc2ab604
posol <dmdmasusapjira1@gmail.com>
2020-05-05 22:36:49 +0500

    add mvc structure in project

---
+++ controllers/users/users_controller.go
@@ -0,0 +1,19 @@
+package users
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented,"implement later")
+}
+
+func GetUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented,"implement later")
+}
+
+func SearchUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented,"implement later")
+}
