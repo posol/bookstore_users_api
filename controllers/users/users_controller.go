84659aa2aabffb64c63580a6a5fb476fe70df200
posol <dmdmasusapjira1@gmail.com>
2020-05-06 14:07:58 +0500

    add simple users service

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,19 +1,47 @@
 package users
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/posol/bookstore_users_api/domain/users"
+	"github.com/posol/bookstore_users_api/services"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented,"implement later")
+	fmt.Println("new request...")
+	var user users.User
+	fmt.Println(user)
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		fmt.Println(err)
+		// TODO: handle error
+		return
+	}
+	if err := json.Unmarshal(bytes, &user); err != nil {
+		fmt.Println(err)
+		// TODO: handle json error
+		return
+	}
+	fmt.Println("user - ", user)
+	fmt.Println(string(bytes), "  ", err)
+	
+	result, err := services.CreateUser(user)
+	if err != nil {
+		fmt.Println(err)
+		// TODO: handle user creation error
+		return
+	}
+	c.JSON(http.StatusCreated,result )
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented,"implement later")
+	c.String(http.StatusNotImplemented, "implement later")
 }
 
 func SearchUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented,"implement later")
+	c.String(http.StatusNotImplemented, "implement later")
 }
---
+++ services/users_service.go
@@ -0,0 +1,7 @@
+package services
+
+import "github.com/posol/bookstore_users_api/domain/users"
+
+func CreateUser(user users.User) (*users.User, error) {
+	return nil, nil
+}
