c4abbb334784f0565f26e9aa596c20ce72fa2225
posol <dmdmasusapjira1@gmail.com>
2020-05-12 13:27:21 +0500

    add users save in memory

--- domain/users/users_dao.go
+++ domain/users/users_dao.go
@@ -10,7 +10,7 @@ var (
 	usersDB = make(map[int64]*User)
 )
 
-func (user User) Get() *errors.RestError {
+func (user *User) Get() *errors.RestError {
 	result := usersDB[user.Id]
 	if result == nil {
 		return errors.NewNotFoundError(fmt.Sprintf("user %d not found ", user.Id))
@@ -25,6 +25,15 @@ func (user User) Get() *errors.RestError {
 	return nil
 }
 
-func (user User) Save() *errors.RestError {
+func (user *User) Save() *errors.RestError {
+	current := usersDB[user.Id]
+	if current != nil {
+		if current.Email == user.Email {
+			return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
+		}
+		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	}
+
+	usersDB[user.Id] = user
 	return nil
 }
--- services/users_service.go
+++ services/users_service.go
@@ -9,5 +9,10 @@ func CreateUser(user users.User) (*users.User, *errors.RestError) {
 	if err := user.Validate(); err != nil {
 		return nil, err
 	}
+
+	if err := user.Save(); err != nil {
+		return nil, err
+	}
+
 	return &user, nil
 }
